4dcd8a14ee885fe9bb3bd2d04dfc96763267729a
Maike <maike_jbe@hotmail.com>
2018-03-20 10:10:13 -0300

    Alterado código devido alterações de layout

--- src/Controllers/Converter.js
+++ src/Controllers/Converter.js
@@ -2,16 +2,19 @@ import {binaryToText, textToBinary} from "../Models/BinaryConverter.js"
 
 window.addEventListener("DOMContentLoaded", () => {
     let $ = document.querySelector.bind(document)
-    let textBox = $(".text-box")
-    $(".form").addEventListener("submit", e => {
+    let form = $(".form")
+
+    form.addEventListener("submit", e => {
         e.preventDefault()
-        let text = binaryToText(textBox.value)
-        console.log(text)
-    })
-    let textBox2 = $(".text-box2")
-    $(".form2").addEventListener("submit", e => {
-        e.preventDefault()
-        let binary = textToBinary(textBox2.value)
-        console.log(binary)
+        let data = new FormData(form)
+        let convertTo = data.get("convert-to")
+        let output;
+        if (convertTo === "Binary") {
+            output = textToBinary(data.get("text"))
+        } else {
+            output = binaryToText(data.get("text"))
+        }
+
+        $('.output-container').innerHTML = `<p>${output}</p>`
     })
 })
